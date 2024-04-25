2326b20be5114b017a21e939781208c4d4861a7e
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2024-04-25 09:15:39 +0200

    try to fix build.zig for zig 0.12 (problem seems to have been this change https://github.com/ziglang/zig/commit/142471fcc46070326526e3976f0150fe734df0b6\#diff-f87bb3596894756629bc39d595fb18d479dc4edf168d93a911cadcb060f10fccR604-R605)

--- build.zig
+++ build.zig
@@ -12,6 +12,8 @@ pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "re",
         .root_source_file = .{ .path = "re.zig" },
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
     b.installArtifact(exe);
 
